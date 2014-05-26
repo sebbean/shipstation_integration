a1be502d00a7043e8f2b1560ef5b252ad124168b
Sean Schofield <sean@spreecommerce.com>
2014-05-26 15:06:54 -0400

    Consolidate the /map_tracking and /get_shipments webhooks
    The /map_tracking stuff as separate webhook was overkill.
    
    [Closes IN-75]

--- shipstation_app.rb
+++ shipstation_app.rb
@@ -43,43 +43,6 @@ class ShipStationApp < EndpointBase::Sinatra::Base
     result 200, "Shipment transmitted to ShipStation: #{@shipstation_id}"
   end
 
-  # Use this to lookup the real id of the order based on the shipstation shipment id and map
-  # the shipment tracking information to it
-  post '/map_tracking' do
-    begin
-      authenticate_shipstation
-
-      @shipment = @payload[:shipment]
-      store_id = @config[:shipstation_store_id]
-
-      @client.Orders.filter("OrderID eq #{@shipment[:order_id]}")
-
-      resource = @client.execute.first
-      @order_number = resource.OrderNumber
-
-      # in some cases we might not be interested in orders from other stores (manual orders, etc.)
-      if store_id.blank? || (store_id.to_i == resource.StoreID)
-        # now we can get the real order number and update with the tracking information
-        add_object :order, {
-          id: @order_number,
-          tracking_number: @shipment[:tracking],
-          shipping_status: "shipped"
-        }
-      else
-        result 200, "Order does not match the specified store id: #{store_id}"
-      end
-
-    rescue => e
-      # tell Honeybadger
-      log_exception(e)
-
-      # tell the hub about the unsuccessful get attempt
-      result 500, "Unable to get order from ShipStation. Error: #{e.message}"
-    end
-
-    result 200, "Order #{@order_number} has shipped with tracking: #{@shipment[:tracking]}"
-  end
-
   post '/get_shipments' do
 
     begin
@@ -93,10 +56,17 @@ class ShipStationApp < EndpointBase::Sinatra::Base
 
       # TODO - get shipping carrier, etc.
       shipstation_result.each do |resource|
+
+        # lookup the shipment (aka order) number that we originally supplied shipstation with
+        @client.Orders.filter("OrderID eq #{resource.OrderID.to_s}")
+
+        order_resource = @client.execute.first
+        shipment_number = order_resource.OrderNumber
+
         add_object :shipment, {
-          id: resource.ShipmentID.to_s,
+          id: shipment_number,
           tracking: resource.TrackingNumber,
-          order_id: resource.OrderID.to_s
+          shipstation_id: resource.ShipmentID.to_s,
         }
       end
       @kount = shipstation_result.count
